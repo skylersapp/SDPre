20b2ac8e3b7ca0d0e9a98d198696e4743c2b292b
skylersapp <skylersapp@icloud.com>
2019-10-26 15:21:01 -0400

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "I, Skyler Sapp, am determined to learn how to code." );
+    System.out.println( "Today's date is Saturday, October 26 2019" );
+  }
+}
