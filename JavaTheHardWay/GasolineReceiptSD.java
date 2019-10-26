dfa0f9be829e242d8be57005aa1edf3c5a082405
skylersapp <skylersapp@icloud.com>
2019-10-26 17:59:55 -0400

    finish jthw GasolineReceipt study drill

---
+++ JavaTheHardWay/GasolineReceiptSD.java
@@ -0,0 +1,17 @@
+public class GasolineReceiptSD{
+  public static void main( String[] args ) {
+    System.out.println( "+------------------------+");
+    System.out.println( "|                        |");
+    System.out.println( "|     Shell Gasoline     |");
+    System.out.println( "|                        |");
+    System.out.println( "| 2019-10-26  05:54PM    |");
+    System.out.println( "|                        |");
+    System.out.println( "| Gallons:       12.000  |");
+    System.out.println( "| Price/gallon: $ 2.409  |");
+    System.out.println( "|                        |");
+    System.out.println( "| Fuel total:  $ 28.91   |");
+    System.out.println( "|                        |");
+    System.out.println( "+------------------------+");
+
+  }
+}
