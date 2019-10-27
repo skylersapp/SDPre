2590d0c535871e5f816ebf891e01a27388be989d
skylersapp <skylersapp@icloud.com>
2019-10-26 20:13:22 -0400

    complete objective1lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main( String[] args ) {
+    System.out.println( "Name : Skyler Sapp" );
+    System.out.println( "Age : 32" );
+    System.out.println( "State : North Carolina" );
+  }
+}
