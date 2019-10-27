e7124e4241148cc157dded71146030d1f6d4b681
skylersapp <skylersapp@icloud.com>
2019-10-26 23:47:10 -0400

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main( String[] args ) {
+    String food1, food2, food3;
+
+    food1 = "Thai Food";
+    food2 = "Fruits and vegetables";
+    food3 = "Seafood";
+
+    System.out.println("My Favorite Foods Are: \n1. " + food1 +
+    "\n2. " + food2 +
+    "\n3. " + food3);
+  }
+}
