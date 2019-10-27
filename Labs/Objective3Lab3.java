dd579b0ce1332f4971c07e0b3e2caafe49292899
skylersapp <skylersapp@icloud.com>
2019-10-27 12:44:06 -0400

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Objective3Lab3 {
+  public static void main( String[] args ){
+    int birthYear, age, currentYear;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old are you?");
+    age = input.nextInt();
+
+
+    currentYear = 2019;
+    birthYear = currentYear - age;
+
+
+    System.out.println("You were born in " + birthYear);
+    input.close();
+
+  }
+}
