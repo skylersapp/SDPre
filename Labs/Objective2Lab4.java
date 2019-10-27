5dd9c9869196303112b7c5b7ba40d975c95e57be
skylersapp <skylersapp@icloud.com>
2019-10-26 23:56:51 -0400

    complete objective2lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,12 @@
+public class Objective2Lab4 {
+  public static void main( String[] args ){
+    double num1, num2, num3, average;
+
+    num1 = 5;
+    num2 = 20;
+    num3 = 80;
+    average = (num1+num2+num3) / 3;
+    System.out.println(average);
+
+  }
+}
