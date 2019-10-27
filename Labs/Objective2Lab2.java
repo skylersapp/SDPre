e9daa615df87ba9e83abcfdbeb23e2d5d1ed6178
skylersapp <skylersapp@icloud.com>
2019-10-26 23:35:43 -0400

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main( String[] args ){
+
+  String name = "Skyler Sapp";
+  int age = 32;
+  String state = "North Carolina";
+
+  System.out.println( "Name: " + name + " Age: " + age + " State: " + state );
+}
+
+}
