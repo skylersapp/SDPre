918379dcd93bd19b10d079c9430f6d95143b9111
skylersapp <skylersapp@icloud.com>
2019-10-27 12:53:01 -0400

    complete objective3lab4

---
+++ Labs/Objective3Lab4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Objective3Lab4 {
+  public static void main( String[] args ) {
+    String firstName;
+    String lastName;
+    int age;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("What is your first name?");
+    firstName = input.next();
+
+    System.out.print("What is your last name?");
+    lastName = input.next();
+
+    System.out.print("What is your age?");
+    age = input.nextInt();
+
+    System.out.println("First Name: " + firstName +
+    ", Last Name: " + lastName + ", Age: " + age);
+    
+
+    input.close();
+
+    }
+  }
