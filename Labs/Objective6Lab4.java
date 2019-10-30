5cc622c676bb9a0843568d0032e5f1a00ff13689
skylersapp <skylersapp@icloud.com>
2019-10-30 03:55:48 -0400

    complete objective6lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,9 @@
+public class Objective6Lab4{
+  public static void main(String[] args){
+    int sum = 0, count = 1;
+
+    while (count < 21) {
+      sum = (sum+(count++));
+    } System.out.println(sum);
+  }
+}
